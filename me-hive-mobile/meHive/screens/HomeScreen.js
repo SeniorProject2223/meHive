b2c060a75d5551df0b0ae5a1a330eba65d890cfa
Anakin <fryanakin@gmail.com>
2022-11-01 12:58:44 -0400

    Start Screen Base Code

--- me-hive-mobile/meHive/screens/HomeScreen.js
+++ me-hive-mobile/meHive/screens/HomeScreen.js
@@ -1,8 +1,37 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Image, View, ScrollView, StyleSheet} from "react-native";
 
-const HomeScreen = () => {
-    return (<Text>Hello!</Text>);
+export default function App () {
+    return (
+        <View style = {StyleSheet.styleHomePage} >
+            <View style = {StyleSheet.styleRectangle2} > </View>
+            <Image style = {StyleSheet.styleDefaultContact1} source = {{uri: "https://nyc3.digitaloceanspaces.com/sizze-storage/media/images/43YolKiFF3CWX940skkhvRMi.png"}} >       </Image>
+            <Image style = {StyleSheet.styleDefaultContact2} source = {{uri: "https://nyc3.digitaloceanspaces.com/sizze-storage/media/images/43YolKiFF3CWX940skkhvRMi.png"}} >       </Image>
+            <Image style = {StyleSheet.styleDefaultContact3} source = {{uri: "https://nyc3.digitaloceanspaces.com/sizze-storage/media/images/43YolKiFF3CWX940skkhvRMi.png"}} >       </Image>
+            <Image style = {StyleSheet.styleDefaultContact4} source = {{uri: "https://nyc3.digitaloceanspaces.com/sizze-storage/media/images/43YolKiFF3CWX940skkhvRMi.png"}} >       </Image>
+            <Image style = {StyleSheet.styleDefaultContact5} source = {{uri: "https://nyc3.digitaloceanspaces.com/sizze-storage/media/images/43YolKiFF3CWX940skkhvRMi.png"}} >       </Image>
+            <Image style = {StyleSheet.styleDefaultContact6} source = {{uri: "https://nyc3.digitaloceanspaces.com/sizze-storage/media/images/43YolKiFF3CWX940skkhvRMi.png"}} >       </Image>
+            <Image style = {StyleSheet.styleMehiveLogo1} source = {{uri: ""}}></Image> 
+            <View style = {StyleSheet.styleRectangle3}></View>
+            <Image style = {StyleSheet.styleInteractLgGroup1} source = {{uri: ""}}></Image>
+            <Image style = {StyleSheet.styleInteractEmailSocial1} source = {{uri: ""}}> </Image>
+            <Image style = {StyleSheet.styleInteractPhone1} source = {{uri: ""}}> </Image>
+            <Image style = {StyleSheet.styleInteractSmGroup2x1} source = {{uri: ""}}> </Image>
+            <Image style = {StyleSheet.styleInteractSmGroup2x2} source = {{uri: ""}}> </Image>
+            <Image style = {StyleSheet.styleSearchIcon} source = {{uri: ""}}></Image>
+            <View style = {StyleSheet.styleRectangle4}></View>
+            <Text style = {StyleSheet.styleInteractions}>
+                Interactions
+            </Text>
+            <Text style = {StyleSheet.styleGroups}> Groups </Text> 
+            <Image style = {StyleSheet.styleGroupSelected1} source = {{uri: ""}}></Image>
+            <Image style = {StyleSheet.styleGroup1} source = {{uri: ""}}></Image>
+            <Text style = {stylesheet.styleAllContacts}>
+                Group Name
+            </Text>
+            <Image style = {stylesheet.styleAddNewContact1} source = {{uri: ""}}></Image>
+
+            
+        </View>
+    )
 }
-
-export default HomeScreen;
