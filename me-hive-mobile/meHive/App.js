4ffac3a78d6812212bca3256be0813c82c42a56b
Anakin <fryanakin@gmail.com>
2022-11-08 18:06:56 -0500

    Demo Home Screen

--- me-hive-mobile/meHive/App.js
+++ me-hive-mobile/meHive/App.js
@@ -6,79 +6,21 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import MainStackNavigator from './navigation/MainStackNavigator.js';
-import { NavigationContainer } from '@react-navigation/native';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// import { NavigationContainer } from '@react-navigation/native';
+import React, { Component } from 'react';
+// import MainStackNavigator from './navigation/MainStackNavigator.js';
+import HomeScreen from './screens/HomeScreen.js';
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <NavigationContainer>
-      <MainStackNavigator/>
-    </NavigationContainer>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+class App extends Component {
+  render() {
+    return (
+      // <NavigationContainer>
+      //   <MainStackNavigator></MainStackNavigator>
+      // </NavigationContainer>
+      <HomeScreen></HomeScreen>
+    );
+  }
+}
 
 export default App;
--- me-hive-mobile/meHive/navigation/MainStackNavigator.js
+++ me-hive-mobile/meHive/navigation/MainStackNavigator.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { enableScreens } from 'react-native-screens';
-import { createStackNavigator } from '@react-navigation/stack';
-
+import { createNativeStackNavigator } from '@react-navigation/stack';
+import NavigationContainer from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen.js';
 
 enableScreens();
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const MainStackNavigator = () => {
   return (
+    <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name='Home' component={HomeScreen} />
       </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
